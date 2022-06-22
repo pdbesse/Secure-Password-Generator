f40fb1de0e0ff2840275b8bcea17e24fcaf705f0
Phillip <phillip.besse@gmail.com>
2022-06-21 23:35:44 -0400

    prompts on button click

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  confirm("Do you want to use lowercase letters?")
+  confirm("Do you want to use uppercase letters?")
+  confirm("Do you want to use numbers?")
+  confirm("Do you want to use special characters?")
+
+  
+ /* if (window.confirm) "Do you want to use lower case letters?"
+  if (window.confirm) "Do you want to use uppercase letters?"
+  if (window.confirm) "Do you want to use numbers?"
+  if (window.confirm) "Do you want to use special characters?" */
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
