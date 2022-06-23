27dcfd0522178087bc26cf8ccbf0f08258acc1f3
Phillip <phillip.besse@gmail.com>
2022-06-22 21:52:52 -0400

    .js organization

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // Assignment Code
 
-// Assign to arrays?
-var generateBtn = document.querySelector("#generate");
 const lowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","UV","W","X","Y","Z"]
 const numbers = ["0","1","2","3","4","5","6","7","8","9"]
 const symbols = ["!","@","#","$","%","^","&","*","(",")","[","]"]
-/* var lowChar = "abcdefghijklmnopqrstuvwxyz"
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0123456789"
-var symbols = "!@#$%^&*()[]" */
+
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword() {
   var confirmLength = prompt("Choose a password length.")
     if (confirmLength < 8) {
       return alert("Password must be between 8 and 128 characters.");
@@ -25,14 +29,9 @@ function writePassword() {
       var confirmSym = confirm("Do you want to use special characters?")
         if (!(confirmLow || confirmUp || confirmNum || confirmSym)) {
           return alert("You must select at least one character set!");
-          /* return writePassword() */
+
       }
-  }
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  } /* if (confirmLow || !(confirmUp || confirmNum || confirmSym)) */
 
 }
 
@@ -42,5 +41,12 @@ function writePassword() {
 
 // Explore callback functions
 
+// Push array for passwords
+
+// Object Strings
+
+// Activity 03.20 README arrays into new array, indexof method
+
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
